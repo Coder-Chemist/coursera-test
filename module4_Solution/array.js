7c9529b5badf921315f8d1f76a03c061b85d23c4
Coder-Chemist <143320208+Coder-Chemist@users.noreply.github.com>
2023-10-01 13:54:38 +0500

    hello

--- module4_Solution/array.js
+++ module4_Solution/array.js
@@ -1,7 +1,5 @@
-// Array of names
 var names = ["John", "Jane", "Jerry", "Jack", "Maria"];
 
-// Function to determine greeting based on the first letter of the name
 function greet(name) {
     if (name.charAt(0).toLowerCase() === 'j') {
         return "Goodbye " + name;
@@ -10,7 +8,6 @@ function greet(name) {
     }
 }
 
-// Loop over the names and print greetings
 for (var i = 0; i < names.length; i++) {
     console.log(greet(names[i]));
 }
